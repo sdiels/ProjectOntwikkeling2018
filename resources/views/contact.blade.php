0ed28036466586360064a2958a1ac526c8837287
JanMoo <32166427+JanMoo@users.noreply.github.com>
2018-04-19 15:50:12 +0200

    contact page aangemaakt

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="contact">
-    <h1>het lukte lukte</h1>
+    <h1>het lukte het lukte het lukte </h1>
+    <p>sweeper niet stelen</p>
     <img src="{{ asset('images/dora_explorer_wallpaper_61.jpg') }}" alt="">
 
 </div>
