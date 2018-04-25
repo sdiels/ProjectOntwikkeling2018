25b083dea87193522c49cfb3b1814f8383d2abb6
JanMoo <32166427+JanMoo@users.noreply.github.com>
2018-04-25 19:58:08 +0200

    lol

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 <div class="contact">
-    <h1>het lukte het lukte het lukte </h1>
-    <p>sweeper niet stelen</p>
-    <img src="{{ asset('images/dora_explorer_wallpaper_61.jpg') }}" alt="">
+   
+    <h1>slachtoffer van seksuele intimidatie?</h1>
+    <p>Wat u kan doen hangt af van de situatie. Maar denk altijd aan uw eigen veiligheid en twijfel niet om hulp in te roepen van omstanders of de politie te (laten) bellen op het noodnummer 101 als de situatie dreigend of erg onveilig wordt.
+Voor niet-dringende hulp of vragen over geweld kan u bellen naar de gratis hulplijn 1712 of de Blauwe Lijn op het nummer 0800 123 12. U kan ook online een afspraak maken met Politie Antwerpen.</p>
+<p> Nog vragen over wat wel en niet kan? Kijk dan eens op deze links.</p>​
+<p>Wat te zeggen tegen vrienden die u iets vragen dat u niet wil doen?<p>
+<p>Bij Tele-Onthaal kan u anoniem uw verhaal kwijt als u het moeilijk hebt, op het nummer 106 of via chat. Jongeren kunnen ook terecht bij het Jongerenadviescentra (JAC) voor een gesprek, afspraak of chat.</p>
+    <h1>getuigen van seksuele intimidatie?</h1>
+    <p>Tussenkomen is niet evident. Elke situatie is verschillend en u zal zelf (snel) moeten inschatten wat te doen. De belangrijkste stelregel is: probeer de ander te helpen zonder uzelf in gevaar te brengen. Bied hulp als iemand u in vertrouwen neemt of als u getuige bent van seksueel grensoverschrijdend gedrag.</p>
+    
+    <h1>dader van seksuele intimidatie</h1>
+    <p>De meeste vrouwen worden minstens één keer in hun leven seksueel lastiggevallen. Onthoud vooral dat seksuele intimidatie altijd ongewenst, onaanvaardbaar en in bepaalde gevallen zelfs strafbaar is.
+
+Bent u toch te ver gegaan? Denk dan eens na over hoe u uw ongewenst gedrag kan bijsturen.Of hebt u vragen over wat wel en niet kan? ​
+
+</p>
+    <h1>ik zit met vragen of wile even praten over seksuele intimidatie?</h1>
+    
 
 </div>
 @endsection
